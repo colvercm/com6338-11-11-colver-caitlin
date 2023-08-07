f00b0b251443bbc03446e0575661940d1396e660
Caitlin Colver <colvercm@gmail.com>
2023-08-06 20:56:57 -0600

    final js code

--- index.js
+++ index.js
@@ -9,7 +9,32 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = (data) => {
+  let output = '';
+  const [{ author, lines, title }] = data;
+
+  // create the title and author sections
+  output +=
+    makeTag('h2')(title) +
+    pipe(makeTag(`em`), makeTag(`h3`))(`by ` + author);
+
+  // create line breaks for each line
+  const joinLines = arr => arr.join('<br/>');
+  const splitLines = str => str.split('<br/><br/>');
+
+  // split stanzas into paragraphs
+  const makeParagraphText = pipe(joinLines, splitLines);
+  const makeParagraph = makeTag(`p`);
+
+  // turn poem into a string of HTML
+  const paraArray = makeParagraphText(lines).map(str => makeParagraph(str));
+  const poemString = paraArray.join('');
+
+  output += poemString;
+
+  return output;
+}
+
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
